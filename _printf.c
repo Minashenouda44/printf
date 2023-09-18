fa96cc437091db82057890bff27a4b5a04c2168c
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-18 14:55:47 +0000

    handle escape characters

--- _printf.c
+++ _printf.c
@@ -18,30 +18,40 @@ int _printf(const char *format, ...)
 	while (format && format[i])
 	{
 		if (format[i] != '%')
-		{
 			characters_printed += _putchar(format[i]);
-		}
 		else if (format[i] == '%')
 		{
-			i++;
-			if (format[i] == 'c')
+			if (format[i + 1] == 'c')
 				characters_printed += print_char(arg);
-			else if (format[i] == 's')
+			else if (format[i + 1] == 's')
 				characters_printed += print_string(arg);
-			else if (format[i] == '%')
+			else if (format[i + 1] == '%')
 				characters_printed += print_percent();
-			else if (format[i] == 'd')
+			else if (format[i + 1] == 'd')
 				characters_printed += print_decimal(arg);
-			else if (format[i] == 'i')
+			else if (format[i + 1] == 'i')
 				characters_printed += print_decimal(arg);
-			else if (format[i] == 'b')
+			else if (format[i + 1] == 'b')
 				characters_printed += print_binary(arg);
-			else if (format[i] == 'u')
+			else if (format[i] + 1 == 'u')
 				characters_printed += print_unsignedint(arg);
 			else
 			{
 				characters_printed += _putchar('%');
-				characters_printed += _putchar(format[i]);
+				characters_printed += _putchar(format[i + 1]);
+			}
+		}
+		else if (format[i] == '\\')
+		{
+			if (format[i + 1] == 'n')
+				characters_printed += _putchar('\n');
+			else if (format[i + 1] == 't')
+				characters_printed += _putchar('\t');
+			else if (format[i + 1] == '\\')
+				characters_printed += _putchar('\\');
+			else
+			{
+				characters_printed += _putchar(format[i + 1]);
 			}
 		}
 		i++;
