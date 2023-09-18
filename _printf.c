4a8144614cec90ef87bb325b8f7cbb67c8870d9a
Rukiyenur <rokiaglal@yahoo.com>
2023-09-18 06:47:18 -0700

    2nd vertion of printg.c

--- _printf.c
+++ _printf.c
@@ -1,90 +1,96 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
+char *int_to_str(int num);
+
 /**
- * get_specifier -  the specifiers characters & the output's function
- * @format: The format string
- *
- * Return: The specifier character or 0 if not found
+ * _printf - printf functions and spcifiers
+ * @format: The format
+ * 
+ * Return: characters printed
  */
-int get_specifier(const char *format)
-{
-	int r = 0;
-	char specifier;
-
-	while (format[r])
-{
-	if (format[r] == '%')
-{
-	r++;
-	while (format[r])
-{
-	if (format[r] == 'c' || format[r] == 's' ||
-	format[r] == '%' || format[r] == 'd' ||
- 	format[r] == 'i' || format[r] == 'b' ||
-	format[r] == 'u')
-{
-	return (format[r]);
-}
-	r++;
-}
-}
-	r++;
-}
-	return (0);
-}
-
 int _printf(const char *format, ...)
 {
-	int r = 0;
-	int characters_printed = 0;
-	va_list arg;
+	unsigned int r = 0, count = 0;
+	va_list args;
 
-	if (format == 0)
-	return (-1);
-    
-	va_start(arg, format);
-
+	va_start(args, format);
 	while (format && format[r])
 {
-	if (format[r] != '%')
+	if (format[r] == '%' && (format[r + 1] == 'c' || format[r + 1] == 's' || format[r + 1] == 'd'))
 {
-	characters_printed += _putchar(format[r]);
-}
-        else if (format[r] == '%')
+	if (format[r + 1] == 'c')
 {
+	char g = (char)va_arg(args, int);
+	count += write(1, &c, 1);
 	r++;
-	char specifier = get_specifier(format + r);
-            
-	switch (specifier)
-{
-	case 'c':
-	characters_printed += print_char(va_arg(arg, int));
-	break;
-	case 's':
-	characters_printed += print_string(va_arg(arg, char *));
-	break;
-	case '%':
-	characters_printed += print_percent();
-	break;
-	case 'd':
-	case 'i':
-	characters_printed += print_decimal(va_arg(arg, int));
-	break;
-	case 'b':
-	characters_printed += print_binary(va_arg(arg, int));
-	break;
-	case 'u':
-	characters_printed += print_unsignedint(va_arg(arg, unsigned int));
-	break;
-	default:
-	break;
 }
-	r += 1;
+	else if (format[r + 1] == 's')
+{
+	char *s = va_arg(args, char *);
+	count += write(1, s, strlen(s));
+	r++;
+}
+	else if (format[r + 1] == 'd')
+{
+	int b = va_arg(args, int);
+	char *str = int_to_str(b);
+	if (str)
+{
+	count += write(1, str, strlen(str));
+	free(str);
 }
 	r++;
 }
-    
-	va_end(arg);
-	return (characters_printed);
+}
+	else if (format[r] == '%' && format[r + 1] == '%')
+{
+	count += write(1, &format[r], 1);
+	r++;
+}
+	else
+{
+	count += write(1, &format[r], 1);
+}
+	r++;
+}
+	va_end(args);
+	return count;
+}
+
+char *int_to_str(int z)
+{
+	char *str;
+	int tmp = z, len = 0;
+	if (z <= 0)
+{
+	len++;
+	tmp = -tmp;
+}
+	while (tmp > 0)
+{
+	len++;
+	tmp /= 10;
+}
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+	return 0;
+	str[len] = '\0';
+	if (z < 0)
+{
+	str[0] = '-';
+	z = -z;
+}
+	else if (z == 0)
+{
+	str[0] = '0';
+}
+	while (z > 0)
+{
+	str[--len] = (z % 10) + '0';
+	z /= 10;
+}
+return str;
 }
