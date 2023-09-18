95232255b42fa0af71b4d9b347781c7917e4acad
minashenouda44 <minanadyshenouda@gmail.com>
2023-09-18 21:35:22 +0000

    specifiers.c

---
+++ specifiers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * specifiers - a function that handle a given specifier
+ * @specifier: specifiers
+ * @arg: argument
+ * Return: characters printed
+ */
+
+int specifiers(char specifier, va_list arg)
+{
+	if (specifier == 'c')
+		return (print_char(arg));
+	else if (specifier == 's')
+		return (print_string(arg));
+	else if (specifier == '%')
+		return (print_percent());
+	else if (specifier == 'd')
+		return (print_decimal(arg));
+	else if (specifier == 'i')
+		return (print_decimal(arg));
+	else if (specifier == 'b')
+		return (print_binary(arg));
+	else if (specifier == 'u')
+		return (print_unsignedint(arg));
+
+	return (0);
+}
