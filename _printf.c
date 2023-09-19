d0755229a3cea586515f75985f13f22c6b40cb68
Rukiyenur <rokiaglal@yahoo.com>
2023-09-19 08:41:50 -0700

    the last update on _printf.c before the deadline

--- _printf.c
+++ _printf.c
@@ -1,155 +1,80 @@
 #include "main.h"
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <string.h>
 
 char *int_to_str(int num);
 
-/**
- * _printf - printf functions and spcifiers
- * @format: The format
- * Return: characters printed
- */
 int _printf(const char *format, ...)
 {
-	unsigned int r = 0, count = 0;
+	unsigned int r = 0, o = 0;
 	va_list args;
 
-	while (format && format[r])
-{
-	if (format[r] == '%')
-{
-	count += escape_cases(format, &r, args);
-}
-	else
-{
-	count += write(1, &format[r], 1);
-}
-	r++;
-}
 	va_start(args, format);
 	while (format && format[r])
 {
-	if (format[r] == '%' && (format[r + 1] == 'c' || format[r + 1] == 's' ||
-    format[r + 1] == 'd' || format[r + 1] == 'o' || format[r + 1] == 'x' || 
-    format[r + 1] == 'X' || format[r + 1] == 'u'))
-
+	if (format[r] == '%' && format[r + 1] != '\0')
 {
-	if (format[r + 1] == 'c')
-{
-	char c = (char)va_arg(args, int);
-	count += write(1, &c, 1);
 	r++;
-}
-        
-	else if (format[r + 1] == 'o')
+	switch (format[r])
 {
-	unsigned int o = va_arg(args, unsigned int);
-	char *str = unsigned_int_to_octal_str(o);
-	if (str)
+	case 'c':
 {
- 	count += write(1, str, strlen(str));
-	free(str);
+	char c = (char) va_arg(args, int);
+	o += write(1, &c, 1);
+	break;
 }
-	r++;
-}
-	else if (format[r + 1] == 'x')
-{
-}
-	else if (format[r + 1] == 'X')
-{
-}
-	else if (format[r + 1] == 'u')
-{
-	unsigned int u = va_arg(args, unsigned int);
-	char *str = unsigned_int_to_str(u);
-	if (str)
-{
-	count += write(1, str, strlen(str));
-	free(str);
-}
-	r++;
-}
-}
-	if (format[r + 1] == 'c')
-{
-	char g = (char)va_arg(args, int);
-	count += write(1, &c, 1);
-
-
-	r++;
-}
-	else if (format[r + 1] == 's')
+	case 's':
 {
 	char *s = va_arg(args, char *);
-	count += write(1, s, strlen(s));
-	r++;
+	o += write(1, s, strlen(s));
+	break;
 }
-	else if (format[r + 1] == 'd')
-{
-	int b = va_arg(args, int);
-	char *str = int_to_str(b);
-	if (str)
-
-{
-	count += write(1, str, strlen(str));
-	free(str);
+	case '%':
+	o += write(1, &format[r], 1);
+	break;
+	default:
+	o += write(1, &format[r - 1], 1);
+	o += write(1, &format[r], 1);
+	break;
 }
-	r++;
-}
-}
-	else if (format[r] == '%' && format[r + 1] == '%')
-{
-	count += write(1, &format[r], 1);
-	r++;
 }
 	else
 {
-	count += write(1, &format[r], 1);
+	o += write(1, &format[r], 1);
 }
 	r++;
 }
-	if (format[r] == 'r')
-	characters_printed += print_reversed(arg);
-	else if (format[r] == 'R')
-	characters_printed += print_R(arg);
 
 	va_end(args);
-	return (count);
+	return (o);
 }
 
 char *int_to_str(int z)
 {
 	char *str;
-	int tmp = z, len = 0;
-	if (z <= 0)
+	int len = 0, tmp = z;
 
-{
-	len++;
-	tmp = -tmp;
-}
+	if (z <= 0)
+	len++, tmp = -tmp;
+	
 	while (tmp > 0)
-{
-	len++;
-	tmp /= 10;
-}
-	str = (char *)malloc((len + 1) * sizeof(char));
+	len++, tmp /= 10;
+
+	str = (char *) malloc((len + 1) * sizeof(char));
 	if (!str)
-	return (0);
+	return (NULL);
+
 	str[len] = '\0';
-	if (z < 0)
-{
-	str[0] = '-';
-	z = -z;
-}
-	else if (z == 0)
-{
+
+	if (z == 0)
 	str[0] = '0';
-}
-	while (z > 0)
+	else
 {
-	str[--len] = (z % 10) + '0';
-	z /= 10;
+	if (z < 0)
+	str[0] = '-', z = -z;
+
+	while (z > 0)
+	str[--len] = (z % 10) + '0', z /= 10;
 }
 	return (str);
 }
