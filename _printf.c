fbb8d49cc81e9dac0fc95eeb5c8cbea7e3bdf244
Rukiyenur <rokiaglal@yahoo.com>
2023-09-19 10:39:48 -0700

    betty testing

--- _printf.c
+++ _printf.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * _printf - a function that produces output according to a format.
+ * _printf - a function that produces output
  * @format: const char formated input
  * Return: characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	int r = 0;
 	int characters_printed = 0;
 
 	va_list(arg);
 	if (format == NULL)
-		return (-1);
+	return (-1);
 	va_start(arg, format);
 
-	for (i = 0 ; format && format[i] != '\0' ; i++)
-	{
-		if (format[i] != '%')
-			characters_printed += _putchar(format[i]);
+	for (r = 0 ; format && format[r] != '\0' ; r++)
+{
+	if (format[r] != '%')
+	characters_printed += _putchar(format[r]);
 
-		else if (format[i + 1] != '\0')
-		{
-			i++;
-			characters_printed += specifiers(format[i], arg);
-		}
+	else if (format[r + 1] != '\0')
+{
+	r++;
+	characters_printed += specifiers(format[r], arg);
+}
 
-		else
-		{
-			characters_printed += _putchar('%');
-			characters_printed += _putchar(format[i]);
-		}
-	}
+	else
+{
+	characters_printed += _putchar('%');
+	characters_printed += _putchar(format[r]);
+}
+}
 	va_end(arg);
 	return (characters_printed);
 }
