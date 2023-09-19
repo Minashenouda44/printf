44686e8fd8710446cab28f052c9111566a8ae6ba
Rukiyenur <rokiaglal@yahoo.com>
2023-09-19 10:47:52 -0700

    betty testing2

--- print_revers.c
+++ print_revers.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 /**
- * print_reversed - Printting reversed 
- * @arg: argument.
- * Return:characters printed.
+ * print_reversed - Printting reversed
+ * @arg: argument
+ * Return:characters printed
  */
 int print_reversed(va_list arg)
 {
@@ -16,10 +16,8 @@ int print_reversed(va_list arg)
 
 	if (!reversed)
 	return (0);
-    
 	for (r = 0; r < len; r++)
 	reversed[r] = str[len - r - 1];
-    
 	reversed[len] = '\0';
 
 	o += write(1, reversed, len);
